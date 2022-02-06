dee02df5f98128fabb36b4ab3746ab9874309177
Aman Mittal <amandeepmittal@live.com>
2022-02-06 17:39:00 +0530

    Update plugins list

--- .hyper.js
+++ .hyper.js
@@ -140,7 +140,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperborder'],
+  plugins: ['hyperborder', 'hyper-snazzy', 'hyper-search'],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
