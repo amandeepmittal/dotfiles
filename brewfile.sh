a51c6c74e677b8024598f6ac606f9aa1936b06a9
Aman Mittal <amandeepmittal@live.com>
2024-10-05 23:51:10 +0530

    Add brewfile

---
+++ brewfile.sh
@@ -0,0 +1,35 @@
+# Specify directory to install
+cask_args appdir: "/Applications"
+
+# Install packages
+tap 'homebrew/bundle'
+tap 'homebrew/cask-fonts'
+brew 'bun'
+brew 'direnv'
+brew 'ffmpeg'
+brew 'git-crypt'
+brew 'git-lfs'
+brew 'git'
+brew 'readline'
+brew 'scrcpy'
+brew 'yarn'
+brew 'watchman'
+brew 'vale'
+brew 'imageoptim'
+brew 'expo-orbit'
+brew 'cocoapods'
+brew 'typos-cli'
+brew "zsh-syntax-highlighting"
+
+
+# Fonts
+cask 'font-jetbrains-mono'
+cask 'font-hack-nerd-font'
+
+# Other apps
+cask 'insomnia'
+cask 'visual-studio-code'
+cask 'google-chrome'
+cask 'google-chrome@canary'
+cask 'brave-browser'
+cask 'arc'
